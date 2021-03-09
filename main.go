454beef1f9a57f5a4abfeb07e17c654a9442b409
Seongmin Kim <shieldnet64@gmail.com>
2021-03-09 23:57:58 +0900

    테스트용 main함수 생성

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package gobit
+package main
+
+import (
+	jwtmaker "github.com/shieldnet/gobit/jwt-maker"
+)
+
+func main() {
+	token := jwtmaker.MakeJWT(`{"key":"value"}`)
+	token2 := jwtmaker.MakeJWT(nil)
+	println(token)
+	println(token2)
+}
