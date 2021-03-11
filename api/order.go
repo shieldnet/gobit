34aa0733440defcf85e14871a7455667aa496a78
Seongmin Kim <atez.dev@gmail.com>
2021-03-12 03:09:32 +0900

    시장가 매도매수 주문관련함수 구현

--- api/order.go
+++ api/order.go
@@ -1,1 +1,52 @@
 package api
+
+import (
+	jwt_maker "github.com/shieldnet/gobit/jwt-maker"
+	"net/url"
+)
+
+const (
+	OrderURL = "https://api.upbit.com/v1/orders"
+)
+
+func BuyOrderByMarketPrice(market, totalPrice string) string {
+	ord_type := "price"
+	side := "bid"
+	data := makeUrlValues(market, side, "", totalPrice, ord_type)
+	header := map[string]string{
+		"Authorization": "Bearer " + jwt_maker.MakeJwtWithPayload(data),
+	}
+	resp, _ := HttpPOST(OrderURL, header, data)
+	return string(resp)
+}
+
+func SellOrderByMarketPrice(market, volume string) string {
+	ord_type := "market"
+	side := "ask"
+	data := makeUrlValues(market, side, volume, "", ord_type)
+	header := map[string]string{
+		"Authorization": "Bearer " + jwt_maker.MakeJwtWithPayload(data),
+	}
+	resp, _ := HttpPOST(OrderURL, header, data)
+	return string(resp)
+}
+
+func makeUrlValues(market, side, volume, price, ord_type string) url.Values {
+	data := url.Values{}
+	if market != "" {
+		data["market"] = []string{market}
+	}
+	if market != "" {
+		data["side"] = []string{side}
+	}
+	if volume != "" {
+		data["volume"] = []string{volume}
+	}
+	if price != "" {
+		data["price"] = []string{price}
+	}
+	if ord_type != "" {
+		data["ord_type"] = []string{ord_type}
+	}
+	return data
+}
