dfffb6a67fa481a183d00674c25d498587cece18
Seongmin Kim <atez.dev@gmail.com>
2021-03-13 03:33:17 +0900

    객체지향 프로그래밍으로 바꾼거 실제 로직에 적용 (go routine)

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import "github.com/shieldnet/gobit/strategy"
+import (
+	"github.com/shieldnet/gobit/strategy"
+	"sync"
+)
 
 const (
 	Tfuel = "KRW-TFUEL"
-	MLK = "KRW-PCI"
+	Ankr  = "KRW-ANKR"
+	Chz   = "KRW-CHZ"
+	Meta  = "KRW-META"
+	Pxl   = "KRW-PXL"
+	Kava  = "KRW-KAVA"
+	Med   = "KRW-MED"
 )
 
 func main() {
-	strategy.Init(MLK)
+	coinList := []string{Tfuel, Ankr, Chz, Meta, Pxl, Kava, Med}
+	var strategies []*strategy.Strategy
+	for _, coin := range coinList {
+		s := &strategy.Strategy{
+			Market:        coin,
+			BuyCandleNum:  15,
+			SellCandleNum: 8,
+			QuitRate:      2.0,
+			CandleUnit:    3,
+			NextState:     "Init",
+			Balance:       "0",
+		}
+		strategies = append(strategies, s)
+	}
+	for true {
+		println("-------------------상황판--------------------")
+		wait := new(sync.WaitGroup)
+		wait.Add(len(strategies))
+		for _, s := range strategies {
+			go s.Execute(wait)
+		}
+		wait.Wait()
+		println("---------------------------------------------")
+	}
 }
