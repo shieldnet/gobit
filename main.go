74221ae3cfd26b5f46bcb7ad81b7dac252583815
Seongmin Kim <atez.dev@gmail.com>
2021-03-13 20:42:38 +0900

    최적화 key값 가져옴

--- main.go
+++ main.go
@@ -8,7 +8,11 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/shieldnet/gobit/strategy"
+	"io/ioutil"
+	"log"
+	"os"
 	"sync"
 )
 
@@ -27,14 +31,28 @@ const (
 	Obsr  = "KRW-OBSR"
 )
 
+type CandleSet struct {
+	Bc int `json:"bc"`
+	Sc int `json:"sc"`
+}
+
 func main() {
+
+	jsonFile, err := os.Open("result.json")
+	if err != nil{
+		log.Panic(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var cinfo map[string]CandleSet
+	json.Unmarshal(byteValue, &cinfo)
+
 	coinList := []string{Tfuel, Ankr, Chz, Pxl, Kava, Solve, Mlk, Obsr}
 	var strategies []*strategy.Strategy
 	for _, coin := range coinList {
 		s := &strategy.Strategy{
 			Market:        coin,
-			BuyCandleNum:  20,
-			SellCandleNum: 20,
+			BuyCandleNum:  cinfo[coin].Bc,
+			SellCandleNum: cinfo[coin].Sc,
 			QuitRate:      2.0,
 			CandleUnit:    5,
 			NextState:     "Init",
