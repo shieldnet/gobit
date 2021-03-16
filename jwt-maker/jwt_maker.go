27998f1f6a4eace8491e338c88a337112f718a93
Seongmin Kim <atez.dev@gmail.com>
2021-03-16 22:07:06 +0900

    type Keys moved to jwt-maker.go

--- jwt-maker/jwt_maker.go
+++ jwt-maker/jwt_maker.go
@@ -16,6 +16,14 @@ import (
 	"net/url"
 )
 
+type Keys struct {
+	Secret []byte `json:"secret"`
+	Access string `json:"access"`
+}
+
+type KeySet []Keys
+
+
 func MakeJwtWithoutPayload(keys Keys) string {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := make(jwt.MapClaims)
