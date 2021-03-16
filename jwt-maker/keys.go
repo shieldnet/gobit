a67509d96f1e7cb31ec114d049b5c6430c0acdee
Seongmin Kim <atez.dev@gmail.com>
2021-03-16 22:51:47 +0900

    Modulization

--- jwt-maker/keys.go
+++ jwt-maker/keys.go
@@ -7,13 +7,6 @@
 
 package jwt_maker
 
-type Keys struct {
-	Secret []byte `json:"secret"`
-	Access string `json:"access"`
-}
-
-type KeySet []Keys
-
 var MainKey = Keys {
 	Secret: []byte("5GxZhUR6tgmgSFqcyyIoUV6OgIsBsEWyMTiBl6OH"),
 	Access: "BA2aG2v5UgcG4mR5Us6P3B9qFlQb8Y6fMD4WMqU7",
