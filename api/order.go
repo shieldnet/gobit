c42186512f91fabd5370d248fa55b32ab1dac71b
Seongmin Kim <atez.dev@gmail.com>
2021-04-03 22:29:17 +0900

    주문(매수, 매도, 주문 리스트, 개별 주문 조회 구현)

--- api/order.go
+++ api/order.go
@@ -18,23 +18,17 @@ const (
 )
 
 func BuyOrderByMarketPrice(market, totalPrice string, key jwtmaker.Keys) string {
-	ord_type := "price"
-	side := "bid"
-	data := makeUrlValues(market, side, "", totalPrice, ord_type)
-	header := map[string]string{
-		"Authorization": "Bearer " + jwtmaker.MakeJwtWithPayload(key, data),
-	}
-	resp, err := HttpPOST(OrderURL, header, data)
-	if err != nil {
-		log.Fatalln(string(resp), err)
-	}
-	return string(resp)
+	resp := Order(market, "bid", "", totalPrice, "price", "", key)
+	return resp
 }
 
 func SellOrderByMarketPrice(market, volume string, key jwtmaker.Keys) string {
-	ord_type := "market"
-	side := "ask"
-	data := makeUrlValues(market, side, volume, "", ord_type)
+	resp := Order(market, "ask", volume, "", "market", "", key)
+	return resp
+}
+
+func Order(market, side, volume, price, ordType, identifier string, key jwtmaker.Keys) string {
+	data := makeUrlValues(market, side, volume, price, ordType, identifier)
 	header := map[string]string{
 		"Authorization": "Bearer " + jwtmaker.MakeJwtWithPayload(key, data),
 	}
@@ -45,7 +39,69 @@ func SellOrderByMarketPrice(market, volume string, key jwtmaker.Keys) string {
 	return string(resp)
 }
 
-func makeUrlValues(market, side, volume, price, ord_type string) url.Values {
+func Buy(market, price, volume string, key jwtmaker.Keys) string {
+	return Order(market, "ask", volume, price, "limit", "", key)
+}
+
+func Sell(market, price, volume string, key jwtmaker.Keys) string {
+	return Order(market, "bid", volume, price, "limit", "", key)
+}
+
+func Cancle(uuid, identifier string, key jwtmaker.Keys) string {
+	data := url.Values{}
+	if uuid != "" {
+		data["uuid"] = []string{uuid}
+	}
+	if identifier != "" {
+		data["identifier"] = []string{identifier}
+	}
+	header := map[string]string{
+		"Authorization": "Bearer " + jwtmaker.MakeJwtWithPayload(key, data),
+	}
+	resp, err := HttpDelete(OrderURL, header, data)
+	if err != nil {
+		log.Fatalln(string(resp), err)
+	}
+	return string(resp)
+}
+
+func GetOrderList(market string, states []string, key jwtmaker.Keys) string {
+	data := url.Values{}
+	if market != "" {
+		data["market"] = []string{market}
+	}
+	if len(states) > 0 {
+		data["market"] = states
+	}
+	header := map[string]string{
+		"Authorization": "Bearer " + jwtmaker.MakeJwtWithPayload(key, data),
+	}
+	resp, err := HttpGet(OrderURL, header, data)
+	if err != nil {
+		log.Fatalln(string(resp), err)
+	}
+	return string(resp)
+}
+
+func GetOrderInfo(uuid, identifier string, key jwtmaker.Keys) string {
+	data := url.Values{}
+	if uuid != "" {
+		data["uuid"] = []string{uuid}
+	}
+	if identifier != "" {
+		data["identifier"] = []string{identifier}
+	}
+	header := map[string]string{
+		"Authorization": "Bearer " + jwtmaker.MakeJwtWithPayload(key, data),
+	}
+	resp, err := HttpGet(OrderURL, header, data)
+	if err != nil {
+		log.Fatalln(string(resp), err)
+	}
+	return string(resp)
+}
+
+func makeUrlValues(market, side, volume, price, ord_type, identifier string) url.Values {
 	data := url.Values{}
 	if market != "" {
 		data["market"] = []string{market}
@@ -61,6 +117,9 @@ func makeUrlValues(market, side, volume, price, ord_type string) url.Values {
 	}
 	if ord_type != "" {
 		data["ord_type"] = []string{ord_type}
+	}
+	if identifier != "" {
+		data["identifier"] = []string{identifier}
 	}
 	return data
 }
